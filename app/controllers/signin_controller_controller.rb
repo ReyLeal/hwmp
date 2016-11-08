90c9dc497a5b1f5b29101d55d9a92484ed5a0b3b
David Martin <hwm@sfldesigners.com>
2016-11-08 11:50:54 -0500

    Added controller to handle signin redirect

---
+++ app/controllers/signin_controller_controller.rb
@@ -0,0 +1,7 @@
+class SigninControllerController < ApplicationController
+  protected
+
+  def after_sign_in_path_for(user)
+    new_photo_path # Or :prefix_to_your_route
+  end
+end
---
+++ app/helpers/signin_controller_helper.rb
@@ -0,0 +1,2 @@
+module SigninControllerHelper
+end
